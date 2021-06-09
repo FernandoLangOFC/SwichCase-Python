9da627e8f2b5bbd290cb68db9f61e1585f6f190e
FernandoLangOFC <84889316+FernandoLangOFC@users.noreply.github.com>
2021-06-08 22:46:52 -0300

    Update exemple.py

--- src/exemple.py
+++ src/exemple.py
@@ -1,11 +1,15 @@
 from SwichCase import Swich
 
 while(True):
+    #Entrada do usuario
     inp = input("Digite um nome: ")
     s = Swich(inp.lower())
+    #Caso o nome que o usuario digitou seja algum desses, eh mostrado na tela.
     s.case("marcos", print, "Você disse Marcos")
     s.case("jhon", print, "Você disse Jhon")
     s.case("bruno", print, "Você disse bruno")
+    
+    #Caso o nome que o usuario digitou nao seja nenhum dos nomes acima, uma funcao padrao eh acionada
     s.default(print, "Nenhum nome correspondente a pesquisa")
     if inp == "0":
         break
